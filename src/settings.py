f2eaa16fba42c34bdbfadcf03f71bea69cf3b079
Karl Rivest Harnois <karlrivestharnois@gmail.com>
2026-01-16 18:50:13 -0500

    Use dataclass for settings

--- src/settings.py
+++ src/settings.py
@@ -1,8 +1,11 @@
+from dataclasses import dataclass
+
+
+@dataclass
 class Settings:
-    def __init__(self, bridge_ip, username, action, light_name, group_name, brightness):
-        self.bridge_ip = bridge_ip
-        self.username = username
-        self.action = action
-        self.light_name = light_name
-        self.group_name = group_name
-        self.brightness = brightness
+    bridge_ip: str
+    username: str | None
+    action: str
+    light_name: str | None
+    group_name: str | None
+    brightness: int | None
